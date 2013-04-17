165172c104087238e8052a564b762f5fbd1f9b30
sustance <kym.michael@gmail.com>
2013-04-17 11:37:57 +0800

    delete lots of files

--- js/jqm-defaults.js
+++
@@ -1,5 +0,0 @@
-// JavaScript Document
-$(document).bind("mobileinit", function(){
-	//remove page transitions as they don't fit this kind of template visually
-    $.mobile.defaultPageTransition = 'none';
-});
