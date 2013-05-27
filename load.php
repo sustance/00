a0ab0112e47d9125562584781d09a12c449ae7df
sustance <kym.michael@gmail.com>
2013-05-27 11:07:22 +0800

    add spinner and dom extract

---
+++ load.php
@@ -0,0 +1,5 @@
+<a href="http://envato.com/" id="picture" target="_blank">
+	<img style="float:left" src="http://sustance.net/ad/sustancelogo.png"></img>
+</a>
+<h3>This is a test</h3>
+<p>These are demos of the ajax setups, json doenot work as google translate broken …</p>
